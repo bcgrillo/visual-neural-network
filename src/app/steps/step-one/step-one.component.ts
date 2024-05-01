4b706e603965060051d87047faca927409867ab4
Bruno Grillo <bcgrillo@yahoo.com>
2024-05-01 23:48:50 +0200

    First test with d5 for drawing

--- src/app/steps/step-one/step-one.component.ts
+++ src/app/steps/step-one/step-one.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { StepTemplateComponent } from '../../components/step-template/step-template.component';
 
 @Component({
   selector: 'app-step-one',
   standalone: true,
-  imports: [ RouterLink, StepTemplateComponent ],
+  imports: [ StepTemplateComponent ],
   templateUrl: './step-one.component.html',
   styleUrl: './step-one.component.scss'
 })
